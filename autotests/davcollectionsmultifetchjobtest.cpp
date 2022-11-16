b76c96ea6cb6095d4d6d69555ad64495df5f701c
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-11-16 15:12:05 +0100

    Tests: drop setting no longer used KDE_FORK_SLAVES env var
    NO_CHANGELOG

--- autotests/davcollectionsmultifetchjobtest.cpp
+++ autotests/davcollectionsmultifetchjobtest.cpp
@@ -17,8 +17,6 @@ Q_DECLARE_METATYPE(KDAV::Protocol)
 
 void DavCollectionsMultiFetchJobTest::initTestCase()
 {
-    // To avoid a runtime dependency on klauncher
-    qputenv("KDE_FORK_SLAVES", "yes");
     // To let ctest exit, we shouldn't start kio_http_cache_cleaner
     qputenv("KIO_DISABLE_CACHE_CLEANER", "yes");
 
--- autotests/davitemfetchjobtest.cpp
+++ autotests/davitemfetchjobtest.cpp
@@ -13,8 +13,6 @@
 
 void DavItemFetchJobTest::initTestCase()
 {
-    // To avoid a runtime dependency on klauncher
-    qputenv("KDE_FORK_SLAVES", "yes");
     // To let ctest exit, we shouldn't start kio_http_cache_cleaner
     qputenv("KIO_DISABLE_CACHE_CLEANER", "yes");
 }
