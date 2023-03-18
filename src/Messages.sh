431b5a94d1ffd924d167b45c40fee5838bf59bee
Luigi Toscano <luigi.toscano@tiscali.it>
2023-03-18 02:57:02 +0100

    Fix the translation template version number (5->6)
    KF5 and KF6 must be co-installable, so change the major version
    number which is part of the translation template name.
    In this specific case the original file had no "5" suffix,
    but the KF6 version should have a different name anyway
    as explained.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libkdav.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" | grep -v '/tests/'` -o $podir/libkdav6.pot
